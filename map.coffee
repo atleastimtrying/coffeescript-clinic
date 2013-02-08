cc320c5a24d8adc232271a7cb1d1a034b226de41
Anders Fisher <atleastimtrying@gmail.com>
2013-02-08 16:05:11 +0000

    latlng change

--- map.coffee
+++ map.coffee
@@ -1,6 +1,6 @@
 window.onload = ->
   display = document.getElementById "gmap"
-  latlng = new google.maps.LatLng 52.6337, 1.2933
+  latlng = new google.maps.LatLng 52.6340, 1.2927
   options = 
     center : latlng
     zoom: 16
--- map.js
+++ map.js
@@ -2,7 +2,7 @@
   window.onload = function() {
     var display, latlng, map, marker, options;
     display = document.getElementById("gmap");
-    latlng = new google.maps.LatLng(52.6337, 1.2933);
+    latlng = new google.maps.LatLng(52.6340, 1.2927);
     options = {
       center: latlng,
       zoom: 16,
