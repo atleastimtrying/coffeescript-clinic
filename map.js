ce843ad6086d9f8719227ca4dba51d21ecded332
Anders Fisher <atleastimtrying@gmail.com>
2014-03-08 12:50:02 +0000

    ipswich session details added

--- map.js
+++ map.js
@@ -1,17 +1,16 @@
-
-  window.onload = function() {
-    var display, latlng, map, marker, options;
-    display = document.getElementById("gmap");
-    latlng = new google.maps.LatLng(52.6340, 1.2927);
-    options = {
-      center: latlng,
-      zoom: 16,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(display, options);
-    return marker = new google.maps.Marker({
-      position: latlng,
-      map: map,
-      title: "CoffeeScript Clinic"
-    });
+window.onload = function() {
+  var display, latlng, map, marker, options;
+  display = document.getElementById("gmap");
+  latlng = new google.maps.LatLng(52.0527533, 1.1601255);
+  options = {
+    center: latlng,
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
+  map = new google.maps.Map(display, options);
+  return marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    title: "CoffeeScript Clinic"
+  });
+};
